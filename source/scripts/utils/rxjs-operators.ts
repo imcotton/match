9a00dd9b75105fad5f7723fe18867e29f74ab740
Cotton Hou <himcotton@gmail.com>
2016-09-26 05:06:50 +0800

    additions for rxjs operation in import

--- source/scripts/utils/rxjs-operators.ts
+++ source/scripts/utils/rxjs-operators.ts
@@ -1,13 +1,32 @@
 
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/fromPromise';
 
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/count';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/max';
+import 'rxjs/add/operator/min';
+import 'rxjs/add/operator/ignoreElements';
+import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/operator/take';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/last';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/zip';
+import 'rxjs/add/operator/pairwise';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/toArray';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/groupBy';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/share';
 
