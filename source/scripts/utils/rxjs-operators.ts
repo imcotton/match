03af22ebc849991eddf77b957e0eb6c8e7413414
Cotton Hou <himcotton@gmail.com>
2016-10-03 19:56:17 +0800

    more rxjs operator

--- source/scripts/utils/rxjs-operators.ts
+++ source/scripts/utils/rxjs-operators.ts
@@ -1,15 +1,23 @@
 
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/range';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/never';
 
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/count';
+import 'rxjs/add/operator/concatMapTo';
+import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/operator/auditTime';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/max';
